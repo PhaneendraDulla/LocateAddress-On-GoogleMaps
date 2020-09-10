2c9da53a7746e39af08a042e0b659b736a899d59
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-10 09:58:38 -0400

    Added Basic Logic & API Key

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBPZo_YF34W-6qkRb3DBIEfjMJG_7mp5UI";
+
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+}
+
+form.addEventListener("submit", searchAddressHandler);
